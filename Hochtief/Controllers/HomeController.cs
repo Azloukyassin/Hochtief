63a3bdef7066cb5060bb350a5f096f43700b0a3c
Med Yassin Azlouk <azlouknour@gmail.com>
2020-07-15 11:53:54 +0200

    Release 15.07.2020

--- Hochtief/Controllers/HomeController.cs
+++ Hochtief/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Hochtief.Controllers
 {
+    //noch bei Testing ! 
     public class HomeController : Controller
     {
         public IActionResult Index()
