9a3683218ccbab14cbbe7e9fd2625a4f793eea8e
Med Yassin Azlouk <azlouknour@gmail.com>
2020-09-10 10:58:45 +0200

    Release 10.09

---
+++ TestProject/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // muss nachschauen 
+        [HttpPost]
+        public ActionResult Autherize(Usertest userModel)
+        {
+            using (MohamedAzloukSandboxEntities db = new MohamedAzloukSandboxEntities())
+            {
+                var userDetails = db.Usertest.Where(x => x.username== userModel.username && x.password == userModel.password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = " Falsch Username oder Password";
+                    return View("Index", userModel);
+                }
+                else
+                    return View("Projects", "Index");
+            }
+            //return View();
+        }
+    }
+}
--- TestProject/Controllers/UserController.cs
+++ TestProject/Controllers/UserController.cs
@@ -1,32 +1,30 @@
-﻿using LoginApp.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI.WebControls.WebParts;
 
-namespace LoginApp.Controllers
+namespace TestProject.Controllers
 {
     public class UserController : Controller
     {
         // GET: User
-        public ActionResult AddOrEdit(int id=0)
+        public ActionResult AddOrEdit(int id = 0)
         {
-            UserTab userModel = new UserTab(); 
+            Usertest userModel = new Usertest();
             return View(userModel);
         }
         [HttpPost]
-        public ActionResult AddOrEdit(UserTab userModel)
+        public ActionResult AddOrEdit(Usertest userModel)
         {
             using (MohamedAzloukSandboxEntities model = new MohamedAzloukSandboxEntities())
             {
-                model.UserTab.Add(userModel);
+                model.Usertest.Add(userModel);
                 model.SaveChanges();
             }
             ModelState.Clear();
             ViewBag.SuccessMessage = "Registration Successful ";
-            return View("AddOrEdit", new UserTab()); 
+            return View("AddOrEdit", new Usertest());
         }
     }
 }
