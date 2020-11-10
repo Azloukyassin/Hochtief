f8240375d8275c4bf94da74058175f4fddb90764
Azlouk Yassin <63109198+Azloukyassin@users.noreply.github.com>
2020-11-10 14:54:07 +0100

    Update LayoutA6AdminController.cs
    @Azloukyassin

--- LoginApp/Controllers/Admin/LayoutA6AdminController.cs
+++ LoginApp/Controllers/Admin/LayoutA6AdminController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 
 namespace LoginApp.Controllers.Admin
 {
+
+// Layout Full Done ! 
     public class LayoutA6AdminController : Controller
     {
         // GET: LayoutA6Admin
